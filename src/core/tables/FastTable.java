af6ee3ebe49a9750bfa36da45ed450d9266ec69c
Kheng Boon Pek <boonpek96@gmail.com>
2016-03-10 12:40:35 +0000

    added feature to remove chart

--- src/core/tables/FastTable.java
+++ src/core/tables/FastTable.java
@@ -8,40 +8,40 @@ public class FastTable {
 	
 	static final int combinations = User.combinations;
 	
-	// arrays to store information	
-	
-	int[] userData;
-	int[] costData;
-	
-	int startHour;
-	
-	int size;
-	
-	public FastTable(int initialCapacity) {
-		userData = new int[initialCapacity * combinations];
-		costData = new int[initialCapacity * combinations];
-	}
-	
-	public void add(short[] count, int[] cost) {
-		if (count.length != combinations || cost.length != combinations)
-			throw new IllegalArgumentException("invalid length");
-		
-		userData[size] = count;
-		costData[size] = cost;
-		
-		size++;
-	}
-	
-	public int[] getCost(int index) {
-		return costData[index];
-	}
-	
-	public short[] getCount(int index) {
-		return userData[index];
-	}
-	
-	public int size() {
-		return size;
-	}
+//	// arrays to store information	
+//	
+//	int[] userData;
+//	int[] costData;
+//	
+//	int startHour;
+//	
+//	int size;
+//	
+//	public FastTable(int initialCapacity) {
+//		userData = new int[initialCapacity * combinations];
+//		costData = new int[initialCapacity * combinations];
+//	}
+//	
+//	public void add(short[] count, int[] cost) {
+//		if (count.length != combinations || cost.length != combinations)
+//			throw new IllegalArgumentException("invalid length");
+//		
+//		System.array
+//		costData[size] = cost;
+//		
+//		size++;
+//	}
+//	
+//	public int[] getCost(int index) {
+//		return costData[index];
+//	}
+//	
+//	public short[] getCount(int index) {
+//		return userData[index];
+//	}
+//	
+//	public int size() {
+//		return size;
+//	}
 
 }
